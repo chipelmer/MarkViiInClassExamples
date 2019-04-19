c757c49921906c7739f5dee5779bbcf0d0c89b18
Chip <chip.elmer@gmail.com>
2019-04-19 13:42:13 -0500

    Some sample methods that use lists and arrays

--- MarkViiInClassExamples/Program.cs
+++ MarkViiInClassExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MarkViiInClassExamples
 {
@@ -6,7 +7,72 @@ namespace MarkViiInClassExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            List<string> guesses = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Please enter a guess:");
+                string guess = Console.ReadLine();
+
+                if (guesses.Contains(guess))
+                {
+                    continue;
+                }
+                else
+                {
+                    guesses.Add(guess);
+                }
+
+                Console.WriteLine("That's a new guess!");
+            }
+
+            Console.WriteLine("You guessed:");
+            foreach (string guess in guesses)
+            {
+                Console.WriteLine(guess);
+            }
+        }
+
+        static bool ContainsAtLeastTwoEvenNumbers(List<int> numbers)
+        {
+            int countOfEvens = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    countOfEvens += 1;
+                }
+
+                if (countOfEvens >= 2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool ContainsAnEight(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (number == 8)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static int SumArray(int[] numbers)
+        {
+            int total = 0;
+            foreach (int number in numbers)
+            {
+                total = total + number;
+            }
+
+            return total;
         }
     }
 }
