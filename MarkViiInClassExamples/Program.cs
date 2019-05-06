8dd6fc5f736208af7ba15e7de3b68d326f157bda
Chip <chip.elmer@gmail.com>
2019-05-06 12:47:58 -0500

    CRUD from C# intro

--- MarkViiInClassExamples/Program.cs
+++ MarkViiInClassExamples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MarkViiInClassExamples
 {
@@ -7,42 +8,35 @@ namespace MarkViiInClassExamples
     {
         static void Main(string[] args)
         {
-            string test1 = "abc"; // should result in "A-Bb-Ccc"
-            string test2 = "xYzM"; // should result in "X-Yy-Zzz-Mmmm"
-            Console.WriteLine(Accum(test1));
-            Console.WriteLine(Accum(test2));
-        }
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            ProductRepository repo = new ProductRepository();
 
-        public static string Accum(string s)
-        {
-            string result = "";
-
-            // Each letter needs to be used. We'll access using this variable `index`.
-            for (int index = 0; index < s.Length; index++)
+            while (true)
             {
-                // Each letter needs to be repeated. We'll use `repeatCount` to control how many times.
-                // The number of repetitions is the position of the letter, which is 1 greater than its index, so `index + 1`.
-                for (int repeatCount = 1; repeatCount <= index + 1; repeatCount++)
-                {
-                    // If it's the first time for a letter, it should be uppercase.
-                    if (repeatCount == 1)
-                    {
-                        // If it's not the very first letter, it should have a hyphen before it.
-                        if (index > 0)
-                        {
-                            result += "-";
-                        }
+                Console.WriteLine();
+                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("1) View Products");
+                Console.WriteLine("2) Insert Product");
+                Console.WriteLine("3) Update Product");
+                Console.WriteLine("4) Delete Product");
+                Console.WriteLine("5) Exit");
 
-                        result += char.ToUpper(s[index]);
-                    }
-                    else // If it's not the first time for a letter, it should be lowercase.
-                    {
-                        result += char.ToLower(s[index]);
-                    }
-                }
+                string response = Console.ReadLine();
+                Console.Clear();
+
+                if (response == "1")
+                    UI.ViewProducts(repo);
+                else if (response == "2")
+                    UI.InsertNewProduct(repo);
+                else if (response == "3")
+                    UI.UpdateExistingProduct(repo);
+                else if (response == "4")
+                    UI.DeleteProduct(repo);
+                else if (response == "5")
+                    break;
+                else
+                    Console.WriteLine("Response not recognized.");
             }
-
-            return result;
         }
     }
 }
