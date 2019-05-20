75735bc7d08a7a1d700d472961baffa2b607eab0
Chip <chip.elmer@gmail.com>
2019-05-20 12:53:42 -0500

    Examples of generics

--- MarkViiInClassExamples/Program.cs
+++ MarkViiInClassExamples/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace MarkViiInClassExamples
 {
@@ -8,35 +7,30 @@ namespace MarkViiInClassExamples
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            ProductRepository repo = new ProductRepository();
+            List<int> numbersList = new List<int>();
+            Stack<int> numbersStack = new Stack<int>();
+            Queue<int> numbersQueue = new Queue<int>();
+            Bag<int> numbersBag = new Bag<int>();
+        }
+    }
 
-            while (true)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Please choose an option:");
-                Console.WriteLine("1) View Products");
-                Console.WriteLine("2) Insert Product");
-                Console.WriteLine("3) Update Product");
-                Console.WriteLine("4) Delete Product");
-                Console.WriteLine("5) Exit");
+    class Bag<T>
+    {
+        private List<T> items = new List<T>();
 
-                string response = Console.ReadLine();
-                Console.Clear();
+        public void PutInBag(T item)
+        {
+            items.Add(item);
+        }
 
-                if (response == "1")
-                    UI.ViewProducts(repo);
-                else if (response == "2")
-                    UI.InsertNewProduct(repo);
-                else if (response == "3")
-                    UI.UpdateExistingProduct(repo);
-                else if (response == "4")
-                    UI.DeleteProduct(repo);
-                else if (response == "5")
-                    break;
-                else
-                    Console.WriteLine("Response not recognized.");
-            }
+        public T GetFromBag()
+        {
+            int randomIndex = new Random().Next(0, items.Count);
+
+            T item = items[randomIndex];
+            items.RemoveAt(randomIndex);
+
+            return item;
         }
     }
 }
